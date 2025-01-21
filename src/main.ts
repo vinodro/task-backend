2f3b271632c1e426a6bcbd74da60297c7f38a4ba
QuikAppTech <>
2025-01-21 08:12:57 +0530

    allowed all the origins

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,7 @@ async function bootstrap() {
 
   // Enabling CORS
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://task-frontend-blush.vercel.app/login',
-    ], // Allows all domains, you can restrict it to specific domains
+    origin: '*', // Allows all domains, you can restrict it to specific domains
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
